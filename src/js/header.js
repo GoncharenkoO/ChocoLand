57de7bb4fb353f0d89dd6156ebb5dd40a0c4af98
Olga Goncharenko <92627264+GoncharenkoO@users.noreply.github.com>
2023-04-22 18:30:27 +0300

    fix

--- src/js/header.js
+++ src/js/header.js
@@ -13,3 +13,11 @@
   }
 })();
 
+
+function checkForSpam(message) {
+  let result;
+  // Change code below this line
+result = message.toLowerCase().includes("spam") ||  message.toLowerCase().includes("sale");
+  // Change code above this line
+  return result;
+}
